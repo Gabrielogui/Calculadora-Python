ec834e0821649e83e5caced84b5bb3437b014a05
Gabrielogui <gabrielcsro@gmail.com>
2024-07-18 11:17:36 -0300

    Código da calculadora

---
+++ Calculadora.py
@@ -0,0 +1,71 @@
+from tkinter import *
+
+calculadora = Tk()
+calculadora.geometry("303x215")
+calculadora.title = ("CALCULADORA")
+
+titulo  = Label(calculadora, text = 'CALCULADORA', font = 20) 
+
+display = Label(calculadora, text = '', width = 30, borderwidth = 5, background = '#d3d3d3', fg = 'black')
+
+# =============   FUNÇOES      ============
+
+def pressionar_botao(valor):
+    global expressao
+    expressao += str(valor)
+    display.config(text=expressao)
+
+def calcular():
+    global expressao
+    try:
+        resultado = str(eval(expressao))
+        display.config(text=resultado)
+        expressao = resultado
+    except:
+        display.config(text='ERRO')
+        expressao = ""
+
+
+def limpar():
+    global expressao
+    expressao = ''
+    display.config(text='')
+
+
+#=======  GRID (FORMA DE MOSTRAR) =======
+
+titulo.grid(row = 0, column = 0, columnspan = 4)
+
+display.grid(row = 1, column = 0, rowspan = 2, columnspan = 4)
+
+# ======= BOTOES =======
+
+botoes = [
+    '1', '2', '3', '/',
+    '4', '5', '6', '*',
+    '7', '8', '9', '-',
+    'c', '0', '=', '+'
+]
+
+row_val = 3
+col_val = 0
+
+limpar()
+
+for botao in botoes:
+    if botao == '=':
+        Button(calculadora, text=botao, bg='red', font=5, width=6, height=1, command=calcular).grid(row=row_val, column=col_val)
+    elif botao == 'c':
+        Button(calculadora, text=botao, bg='red', font=5, width=6, height=1, command=limpar).grid(row=row_val, column=col_val)
+    else:
+        Button(calculadora, text=botao, bg='white', font=5, width=6, height=1, command=lambda b=botao: pressionar_botao(b)).grid(row=row_val, column=col_val)
+
+    col_val += 1
+    if col_val > 3:
+        col_val = 0
+        row_val += 1
+
+
+# ======= MAINLOOP =======
+
+calculadora.mainloop()
