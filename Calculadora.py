45d8c1482fd81f4d258702d62bed1c40669c453c
Gabrielogui <gabrielcsro@gmail.com>
2024-07-18 11:29:26 -0300

    Comentando o código

--- Calculadora.py
+++ Calculadora.py
@@ -1,5 +1,6 @@
 from tkinter import *
 
+# |=======| INSTANCIANDO A JANAELA |=======|
 calculadora = Tk()
 calculadora.geometry("303x215")
 calculadora.title = ("CALCULADORA")
@@ -8,37 +9,39 @@ titulo  = Label(calculadora, text = 'CALCULADORA', font = 20)
 
 display = Label(calculadora, text = '', width = 30, borderwidth = 5, background = '#d3d3d3', fg = 'black')
 
-# =============   FUNÇOES      ============
+# |=======| FUNÇOES |=======|
 
+# ======= APERTAR O BOTÃO - FAZER APARECER NO DISPLAY =======
 def pressionar_botao(valor):
     global expressao
     expressao += str(valor)
     display.config(text=expressao)
 
+# ======= FUNÇÃO DE CALCULAR =======
 def calcular():
     global expressao
     try:
-        resultado = str(eval(expressao))
+        resultado = str(eval(expressao)) # eval() -> função que pega expressão e realiza o calulo
         display.config(text=resultado)
         expressao = resultado
     except:
         display.config(text='ERRO')
         expressao = ""
 
-
+# ======= FUNÇÃO PARA LIMPAR DISPLAY =======
 def limpar():
     global expressao
     expressao = ''
     display.config(text='')
 
 
-#=======  GRID (FORMA DE MOSTRAR) =======
+# |=======| GRID (FORMA DE MOSTRAR) |=======|
 
 titulo.grid(row = 0, column = 0, columnspan = 4)
 
 display.grid(row = 1, column = 0, rowspan = 2, columnspan = 4)
 
-# ======= BOTOES =======
+# |=======| BOTOES |=======|
 
 botoes = [
     '1', '2', '3', '/',
@@ -50,8 +53,9 @@ botoes = [
 row_val = 3
 col_val = 0
 
-limpar()
+limpar() # CHAMANDO FUNÇÃO PARA LIMPAR DISPLAY
 
+# ======= COLOCANDO OS BOTÕES =======
 for botao in botoes:
     if botao == '=':
         Button(calculadora, text=botao, bg='red', font=5, width=6, height=1, command=calcular).grid(row=row_val, column=col_val)
@@ -60,12 +64,13 @@ for botao in botoes:
     else:
         Button(calculadora, text=botao, bg='white', font=5, width=6, height=1, command=lambda b=botao: pressionar_botao(b)).grid(row=row_val, column=col_val)
 
+    # COLUNAS E LINHAS
     col_val += 1
     if col_val > 3:
         col_val = 0
         row_val += 1
 
 
-# ======= MAINLOOP =======
+# # |=======| MAINLOOP |=======|
 
 calculadora.mainloop()
